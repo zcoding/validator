4c9b32413416ace61ef69dd73589a220c7ad1cc8
zcoding <wuzijie_1991@163.com>
2015-08-04 11:58:04 +0800

    async support

---
+++ scripts/todo4.js
@@ -0,0 +1,37 @@
+// 异步方式
+var $form = document.getElementById('myForm');
+var checkMe = new FormValidator($form, {
+  field: 'name',
+  rules: {
+    if: 'au',
+    fail: function() { // async
+      // todo
+    }
+  }
+});
+
+checkMe.add({
+  name: 'au',
+  rule: function(value) {
+    return Promise.resolve($.ajax({ // 返回一个Promises/A+规范的Promise
+      url: '/au',
+      type: 'POST',
+      data: {
+        name: value
+      }
+    }));
+  }
+});
+
+// 非配置的方式
+Promise.resolve($.ajax({ // 返回一个Promises/A+规范的Promise
+  url: '/au',
+  type: 'POST',
+  data: {
+    name: value
+  }
+})).then(function success() {
+  // success
+}, function failed() {
+  // fail
+})
